629e34f92dc4d9bcf54c9341b9adbe92c329ce6b
Cecile <yin.cecile@hotmail.com>
2023-03-25 12:28:37 +0100

    Update hello-view.fxml + add in HelloController.java

--- src/main/java/com/librairie/librairie/HelloController.java
+++ src/main/java/com/librairie/librairie/HelloController.java
@@ -2,10 +2,27 @@ package com.librairie.librairie;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private TextField titre;
+    @FXML
+    private TextField auteur;
+    @FXML
+    private TextField presentation;
+    @FXML
+    private TextField parution;
+
+    @FXML
+    private TextField colonne;
+
+    @FXML
+    private TextField rangee;
+
 
     @FXML
     protected void onHelloButtonClick() {
