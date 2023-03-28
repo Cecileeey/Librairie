d1b377c1a78ce39f70c077303845632cc7e373f4
Cecile <yin.cecile@hotmail.com>
2023-03-28 17:29:32 +0200

    Set id (hello-view.fxml)

--- src/main/java/com/librairie/librairie/HelloApplication.java
+++ src/main/java/com/librairie/librairie/HelloApplication.java
@@ -12,12 +12,12 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 1200);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }
