9d13898f3ead4f084eb0e56109fa06d8ffbf236e
Cecile <yin.cecile@hotmail.com>
2023-06-15 10:10:55 +0200

    config

--- src/test/java/librairieTest/Controller/HelloControllerTest.java
+++ src/test/java/librairieTest/Controller/HelloControllerTest.java
@@ -2,7 +2,7 @@ package librairieTest.Controller;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(MockitoExtension.class)
 public class HelloControllerTest {
 
 }
